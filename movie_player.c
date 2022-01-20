66d6205e876531f0b19879b1d074bbbf82f98d75
Linus Brogan <linusbrogan@lclark.edu>
2022-01-20 10:20:42 -0800

    Update movie_player arguments
    Per Jeff's specification, movie_player generates names from the given prefix and bounds.

--- movie_player.c
+++ movie_player.c
@@ -2,19 +2,29 @@
 
 int main(int argc, char *argv[]) {
 	// Check arguments
-	if (argc < 4) {
-		printf("usage: pgm_name width height frame1 [frame2 [...]]\n");
+	if (argc < 6) {
+		printf("usage: pgm window_width window_height prefix_name start_num end_num\n");
 		exit(0);
 	}
 
-	// Initialize graphics
-	int width = atoi(argv[1]);
-	int height = atoi(argv[2]);
-	G_init_graphics(width, height);
+	// Parse arguments
+	int window_width = atoi(argv[1]);
+	int window_height = atoi(argv[2]);
+	char *prefix_name = argv[3];
+	int start_num = atoi(argv[4]);
+	int end_num = atoi(argv[5]);
 
-	// Display frames
-	for (int i = 3; i < argc; i++) {
-		G_get_image_from_file(argv[i], 0, 0) ;
-		G_wait_key();
+	// Initialize graphics
+	G_init_graphics(window_width, window_height);
+
+	// Contruct file names and render frames
+	char file_name[100];
+	while (1) {
+		for (int i = start_num; i <= end_num; i++) {
+			sprintf(file_name, "%s%04d.xwd", prefix_name, i);
+			G_get_image_from_file(file_name, 0, 0) ;
+			char keypress = G_wait_key();
+			if (keypress == 'q') exit(0);
+		}
 	}
 }
