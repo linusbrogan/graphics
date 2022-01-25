d94ac9d6410f85db7279bd030ac7b6400a3eccbc
Linus Brogan <linusbrogan@lclark.edu>
2022-01-25 14:53:35 -0800

    Implement falling stick figure movie

--- stickfigureS.c
+++ stickfigureS.c
@@ -32,11 +32,57 @@ int main(int argc, char **argv)
  // M3d_make_movement_sequence_matrix  function :)
 
  // .....
+// recenter at O, scale, recenter at mid
+	int mtype[100];
+	double mparam[100];
+	int n = 0;
+	mtype[n] = TX;	mparam[n] = -200;	n++;
+	mtype[n] = TY;	mparam[n] = -200;	n++;
+	double scale = winsize / 200;
+	mtype[n] = SX;	mparam[n] = scale;	n++;
+	mtype[n] = SY;	mparam[n] = scale;	n++;
+	double center = winsize / 2;
+	mtype[n] = TX;	mparam[n] = center;	n++;
+	mtype[n] = TY;	mparam[n] = center;	n++;
 
- 
+	double recenter[4][4];
+	double recenter_i[4][4];
+	M3d_make_movement_sequence_matrix(recenter, recenter_i, n, mtype, mparam);
+	M3d_mat_mult_points(x, y, z, recenter, x, y, z, 13);
+
+	n = 0;
+	mtype[n] = TX;	mparam[n] = -center;	n++;
+	mtype[n] = TY;	mparam[n] = -center;	n++;
+	mtype[n] = SX;	mparam[n] = 0.9;	n++;
+	mtype[n] = SY;	mparam[n] = 0.9;	n++;
+	mtype[n] = RZ;	mparam[n] = 10;	n++;
+	mtype[n] = TX;	mparam[n] = center;	n++;
+	mtype[n] = TY;	mparam[n] = center;	n++;
+
+	double fall[4][4];
+	double fall_i[4][4];
+	M3d_make_movement_sequence_matrix(fall, fall_i, n, mtype, mparam);
+
+
  // now make the movie the rotates and shrinks about the center :
+//spin + direction, shrinking until very small, then red circle
+	int i = 0;
+	while (i < 50) {
+		G_rgb(0, 0, 0);
+		G_clear();
 
+		G_rgb(0.25, 0.5, 1);
+		G_fill_polygon(x, y, 13);
 
+		G_wait_key();
+		M3d_mat_mult_points(x, y, z, fall, x, y, z, 13);
+		i++;
+	}
+	G_rgb(0, 0, 0);
+	G_clear();
+	G_rgb(1, 0, 0);
+	G_fill_circle(center, center, center / 6);
+	while (1) G_wait_key();
  // .....
 
 }
