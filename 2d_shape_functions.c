ca3ad03321007d0e8c8dcb3512a4824bcf44dd93
Linus Brogan <linusbrogan@lclark.edu>
2022-02-09 19:09:20 -0800

    Refactor 2D graphing code

--- 2d_shape_functions.c
+++ 2d_shape_functions.c
@@ -1,66 +1,38 @@
-double (*circle_x)(double) = cos;
-double (*circle_y)(double) = sin;
-
-double id(double x) {
+double identity(double x) {
 	return x;
-}
-
-double (*sum4_x)(double) = id;
-double sum4_y(double u) {
-	return pow(1 - u * u * u * u, 0.25);
-}
-
-double square_x(double u) {
-	if (u >= 0 && u < 1) {
-		double w = u;
-		double x = 1 - w;
-		return x;
-	}
-	if (u >= 1 && u < 2) {
-		double w = u - 1;
-		double x = -w;
-		return x;
-	}
-	if (u >= 2 && u < 3) {
-		double w = u - 2;
-		double x = w - 1;
-		return x;
-	}
-	if (u >= 3 && u <= 4) {
-		double w = u - 3;
-		double x = w;
-		return x;
-	}
-	return 0;
-}
-
-double square_y(double u) {
-	if (u >= 0 && u < 1) {
-		double w = u;
-		double y = w;
-		return y;
-	}
-	if (u >= 1 && u < 2) {
-		double w = u - 1;
-		double y = 1 - w;
-		return y;
-	}
-	if (u >= 2 && u < 3) {
-		double w = u - 2;
-		double y = -w;
-		return y;
-	}
-	if (u >= 3 && u <= 4) {
-		double w = u - 3;
-		double y = w - 1;
-		return y;
-	}
-	return 0;
 }
 
 double sgn(double x) {
 	if (x < 0) return -1;
 	if (x > 0) return 1;
+	return 0;
+}
+
+double (*circle_x)(double) = cos;
+double (*circle_y)(double) = sin;
+
+double (*sum4_x)(double) = identity;
+double sum4_y(double u) {
+	return pow(1 - u * u * u * u, 0.25);
+}
+
+double square_x(double u) {
+	if (u >= 0 && u < 2)
+		return 1 - u;
+	if (u >= 2 && u <= 4)
+		return u - 3;
+
+	return 0;
+}
+
+double square_y(double u) {
+	if (u >= 0 && u < 1)
+		return u;
+	if (u >= 1 && u < 3)
+		return 2 - u;
+	if (u >= 3 && u <= 4)
+		return u - 4;
+
 	return 0;
 }
 
@@ -83,7 +55,7 @@ double astroid_y(double u) {
 double (*hyperbola_x)(double) = cosh;
 double (*hyperbola_y)(double) = sinh;
 
-double (*parabola_x)(double) = id;
+double (*parabola_x)(double) = identity;
 double parabola_y(double u) {
 	return u * u;
 }
@@ -93,7 +65,6 @@ double lemon_x(double u) {
 }
 
 double (*lemon_y)(double) = sin;
-
 
 double brachistochrone_x(double u) {
 	return u - sin(u);
--- shapes.c
+++ shapes.c
@@ -7,232 +7,168 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT (WINDOW_WIDTH)
 
-void plot_shape(double (*fx)(double), double (*fy)(double), double low, double high, double *x, double *y, int points) {
-	double dt = (high - low) / (points - 1 + 1); // TODO -1 is better
-	double u = low;
+void zero_fill(double *x, int size) {
+	for (int i = 0; i < size; i++)
+		x[i] = 0;
+}
+
+void generate_points(
+	double (*f_x)(double),
+	double (*f_y)(double),
+	double u_start,
+	double u_end,
+	double *x,
+	double *y,
+	int points
+) {
+	double du = (u_end - u_start) / points;
+	double u = u_start;
 	for (int i = 0; i < points; i++) {
-		x[i] = fx(u);
-		y[i] = fy(u);
-		u += dt;
+		x[i] = f_x(u);
+		y[i] = f_y(u);
+		u += du;
 	}
 }
 
-void graph(double *x, double *y, int points) {
-	for (int i = 0; i < points; i++) {
+void plot_points(double *x, double *y, int points) {
+	for (int i = 0; i < points; i++)
 		G_point(x[i], y[i]);
-	}
+}
+
+void graph_shape(
+	double (*f_x)(double),
+	double (*f_y)(double),
+	double u_start,
+	double u_end,
+	double T[4][4]
+) {
+	double x[NUM_PTS];
+	double y[NUM_PTS];
+	double z[NUM_PTS];
+	zero_fill(z, NUM_PTS);
+	generate_points(f_x, f_y, u_start, u_end, x, y, NUM_PTS);
+	M3d_mat_mult_points(x, y, z, T, x, y, z, NUM_PTS);
+	plot_points(x, y, NUM_PTS);
 }
 
 int main() {
+	// Initialize screen
 	G_init_graphics(WINDOW_WIDTH, WINDOW_HEIGHT);
 	G_rgb(0, 0, 0);
 	G_clear();
 
-	double x[NUM_SHAPES][NUM_PTS];
-	double y[NUM_SHAPES][NUM_PTS];
-	double z[NUM_SHAPES][NUM_PTS];
-
-	int Tn = 0;
-	int Ttypelist[100];
-	double Tvlist[100];
+	// Create movement sequence
+	int T_n = 0;
+	int T_type[100];
+	double T_param[100];
 	double A[4][4];
 	double A_i[4][4];
 
-	int shape = 0;
-
+	// Graph part of a circle from pi/4 to 3pi/2
 	G_rgb(1, 0, 0);
-	plot_shape(cos, sin, 0.25 * M_PI, 1.5 * M_PI, x[shape], y[shape], NUM_PTS);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// circle :
-//   x^2 + y^2 = 1
-//
-//   u in [0,2*M_PI] gives the entire circle :
-//   x = cos(u) ;  y = sin(u) ; 
-  Tn = 0 ; 
-  Ttypelist[Tn] = SX ; Tvlist[Tn] =   50.0 ; Tn++ ;
-  Ttypelist[Tn] = SY ; Tvlist[Tn] =  100.0 ; Tn++ ;
-  Ttypelist[Tn] = TX ; Tvlist[Tn] =  300.0 ; Tn++ ;
-  Ttypelist[Tn] = TY ; Tvlist[Tn] =  500.0 ; Tn++ ;
-// Graph only the part with u in [0.25*M_PI, 1.5*M_PI]
-	M3d_make_movement_sequence_matrix(A, A_i, Tn, Ttypelist, Tvlist);
-	M3d_mat_mult_points(x[shape], y[shape], z[shape], A, x[shape], y[shape], z[shape], NUM_PTS);
-	graph(x[shape], y[shape], NUM_PTS);
+	T_n = 0;
+	T_type[T_n] = SX;	T_param[T_n] =	50.0;	T_n++;
+	T_type[T_n] = SY;	T_param[T_n] =	100.0;	T_n++;
+	T_type[T_n] = TX;	T_param[T_n] =	300.0;	T_n++;
+	T_type[T_n] = TY;	T_param[T_n] =	500.0;	T_n++;
+	M3d_make_movement_sequence_matrix(A, A_i, T_n, T_type, T_param);
+	graph_shape(circle_x, circle_y, 0.25 * M_PI, 1.5 * M_PI, A);
 	G_wait_key();
-	shape++;
 
+	// Graph sum4 from -1 to 1
 	G_rgb(0, 1, 0);
-	plot_shape(sum4_x, sum4_y, -1, 1, x[shape], y[shape], NUM_PTS);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// sum4 :
-//   x^4 + y^4 = 1
-//
-//   u in [-1,1] gives the upper branch of the curve :
-//   x = u  ;  y = pow(1 - u*u*u*u, 0.25) ; 
-  Tn = 0 ; 
-  Ttypelist[Tn] = SX ; Tvlist[Tn] =   30.0 ; Tn++ ;
-  Ttypelist[Tn] = SY ; Tvlist[Tn] =   30.0 ; Tn++ ;
-  Ttypelist[Tn] = TX ; Tvlist[Tn] =  250.0 ; Tn++ ;
-  Ttypelist[Tn] = TY ; Tvlist[Tn] =  170.0 ; Tn++ ;
-// Graph the entire curve.
-	M3d_make_movement_sequence_matrix(A, A_i, Tn, Ttypelist, Tvlist);
-	M3d_mat_mult_points(x[shape], y[shape], z[shape], A, x[shape], y[shape], z[shape], NUM_PTS);
-	graph(x[shape], y[shape], NUM_PTS);
+	T_n = 0;
+	T_type[T_n] = SX;	T_param[T_n] = 30.0;	T_n++;
+	T_type[T_n] = SY;	T_param[T_n] = 30.0;	T_n++;
+	T_type[T_n] = TX;	T_param[T_n] = 250.0;	T_n++;
+	T_type[T_n] = TY;	T_param[T_n] = 170.0;	T_n++;
+	M3d_make_movement_sequence_matrix(A, A_i, T_n, T_type, T_param);
+	graph_shape(sum4_x, sum4_y, -1, 1, A);
 	G_wait_key();
-	shape++;
 
-
+	// Graph a square
 	G_rgb(0, 0, 1);
-	plot_shape(square_x, square_y, 0, 4, x[shape], y[shape], NUM_PTS);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// square :
-//   |x| + |y| = 1
-//
-//   u in [0,4] gives the entire square :
-//     u in [0,1],  w = u     ;  x = 1 - w ;  y = w     ; 
-//     u in [1,2],  w = u - 1 ;  x = -w    ;  y = 1 - w ;    
-//     u in [2,3],  w = u - 2 ;  x = w - 1 ;  y = -w    ;
-//     u in [3,4],  w = u - 3 ;  x = w     ;  y = w - 1 ;
-  Tn = 0 ; 
-  Ttypelist[Tn] = SX ; Tvlist[Tn] =  150.0 ; Tn++ ;
-  Ttypelist[Tn] = SY ; Tvlist[Tn] =   70.0 ; Tn++ ;
-  Ttypelist[Tn] = TX ; Tvlist[Tn] =  500.0 ; Tn++ ;
-  Ttypelist[Tn] = TY ; Tvlist[Tn] =  460.0 ; Tn++ ;
-// Graph the entire curve.
-  	M3d_make_movement_sequence_matrix(A, A_i, Tn, Ttypelist, Tvlist);
-	M3d_mat_mult_points(x[shape], y[shape], z[shape], A, x[shape], y[shape], z[shape], NUM_PTS);
-	graph(x[shape], y[shape], NUM_PTS);
+	T_n = 0;
+	T_type[T_n] = SX;	T_param[T_n] = 150.0;	T_n++;
+	T_type[T_n] = SY;	T_param[T_n] = 70.0;	T_n++;
+	T_type[T_n] = TX;	T_param[T_n] = 500.0;	T_n++;
+	T_type[T_n] = TY;	T_param[T_n] = 460.0;	T_n++;
+ 	M3d_make_movement_sequence_matrix(A, A_i, T_n, T_type, T_param);
+	graph_shape(square_x, square_y, 0, 4, A);
 	G_wait_key();
-	shape++;
 
-
+	// Graph a square
 	G_rgb(0.75, 0.5, 1);
-	plot_shape(square2_x, square2_y, 0, 2 * M_PI, x[shape], y[shape], NUM_PTS);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// square (alternate parameterization) :
-//   |x| + |y| = 1
-//
-//   u in [0,2*M_PI] gives the entire square :
-//   x = sgn(cos(u))*[cos(u)]^2 ;   y = sgn(sin(u))*[sin(u)]^2  ;
-  Tn = 0 ; 
-  Ttypelist[Tn] = SX ; Tvlist[Tn] =  150.0 ; Tn++ ;
-  Ttypelist[Tn] = SY ; Tvlist[Tn] =   70.0 ; Tn++ ;
-  Ttypelist[Tn] = TX ; Tvlist[Tn] =  500.0 ; Tn++ ;
-  Ttypelist[Tn] = TY ; Tvlist[Tn] =  670.0 ; Tn++ ;
-// Graph the entire curve.
-	M3d_make_movement_sequence_matrix(A, A_i, Tn, Ttypelist, Tvlist);
-	M3d_mat_mult_points(x[shape], y[shape], z[shape], A, x[shape], y[shape], z[shape], NUM_PTS);
-	graph(x[shape], y[shape], NUM_PTS);
+	T_n = 0;
+	T_type[T_n] = SX;	T_param[T_n] = 150.0;	T_n++;
+	T_type[T_n] = SY;	T_param[T_n] = 70.0;	T_n++;
+	T_type[T_n] = TX;	T_param[T_n] = 500.0;	T_n++;
+	T_type[T_n] = TY;	T_param[T_n] = 670.0;	T_n++;
+	M3d_make_movement_sequence_matrix(A, A_i, T_n, T_type, T_param);
+	graph_shape(square2_x, square2_y, 0, 2 * M_PI, A);
 	G_wait_key();
-	shape++;
 
-
+	// Graph an astroid
 	G_rgb(1, 1, 0);
-	plot_shape(astroid_x, astroid_y, 0, 2 * M_PI, x[shape], y[shape], NUM_PTS);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// astroid :
-//   sqrt(|x|) + sqrt(|y|) = 1
-//
-//   u in [0,2*M_PI] gives the entire astroid :
-//   x = sgn(cos(u))*[cos(u)]^4 ;  y = sgn(sin(u))*[sin(u)]^4  ;
-  Tn = 0 ; 
-  Ttypelist[Tn] = SX ; Tvlist[Tn] =   80.0 ; Tn++ ;
-  Ttypelist[Tn] = SY ; Tvlist[Tn] =   40.0 ; Tn++ ;
-  Ttypelist[Tn] = RZ ; Tvlist[Tn] =   45.0 ; Tn++ ;
-  Ttypelist[Tn] = TX ; Tvlist[Tn] =  130.0 ; Tn++ ;
-  Ttypelist[Tn] = TY ; Tvlist[Tn] =  650.0 ; Tn++ ;
-// Graph the entire curve.
-	M3d_make_movement_sequence_matrix(A, A_i, Tn, Ttypelist, Tvlist);
-	M3d_mat_mult_points(x[shape], y[shape], z[shape], A, x[shape], y[shape], z[shape], NUM_PTS);
-	graph(x[shape], y[shape], NUM_PTS);
+	T_n = 0;
+	T_type[T_n] = SX;	T_param[T_n] = 80.0;	T_n++;
+	T_type[T_n] = SY;	T_param[T_n] = 40.0;	T_n++;
+	T_type[T_n] = RZ;	T_param[T_n] = 45.0;	T_n++;
+	T_type[T_n] = TX;	T_param[T_n] = 130.0;	T_n++;
+	T_type[T_n] = TY;	T_param[T_n] = 650.0;	T_n++;
+	M3d_make_movement_sequence_matrix(A, A_i, T_n, T_type, T_param);
+	graph_shape(astroid_x, astroid_y, 0, 2 * M_PI, A);
 	G_wait_key();
-	shape++;
 
-
+	// Graph part of a hyperbola from -1 to 1.5
 	G_rgb(0, 1, 1);
-	plot_shape(hyperbola_x, hyperbola_y, -1, 1.5, x[shape], y[shape], NUM_PTS);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// hyperbola :
-//   x^2 - y^2 = 1 
-//
-//   u in (-inf,inf) gives the right branch :
-//   x = cosh(u) ;   y = sinh(u) ;
-  Tn = 0 ; 
-  Ttypelist[Tn] = SX ; Tvlist[Tn] =   70.0 ; Tn++ ;
-  Ttypelist[Tn] = SY ; Tvlist[Tn] =   70.0 ; Tn++ ;
-  Ttypelist[Tn] = TX ; Tvlist[Tn] =  250.0 ; Tn++ ;
-  Ttypelist[Tn] = TY ; Tvlist[Tn] =  150.0 ; Tn++ ;
-// Graph only the part with u in [-1, 1.5]  
-	M3d_make_movement_sequence_matrix(A, A_i, Tn, Ttypelist, Tvlist);
-	M3d_mat_mult_points(x[shape], y[shape], z[shape], A, x[shape], y[shape], z[shape], NUM_PTS);
-	graph(x[shape], y[shape], NUM_PTS);
+	T_n = 0;
+	T_type[T_n] = SX;	T_param[T_n] = 70.0;	T_n++;
+	T_type[T_n] = SY;	T_param[T_n] = 70.0;	T_n++;
+	T_type[T_n] = TX;	T_param[T_n] = 250.0;	T_n++;
+	T_type[T_n] = TY;	T_param[T_n] = 150.0;	T_n++;
+	M3d_make_movement_sequence_matrix(A, A_i, T_n, T_type, T_param);
+	graph_shape(hyperbola_x, hyperbola_y, -1, 1.5, A);
 	G_wait_key();
-	shape++;
 
-
+	// Graph part a parabola from -1 to 1
 	G_rgb(1, 0, 1);
-	plot_shape(parabola_x, parabola_y, -1, 2, x[shape], y[shape], NUM_PTS);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// parabola :
-//   y = x^2
-//
-//   u in (inf,inf) gives the entire parabola : 
-//   x = u ;  y  = u*u ;
-  Tn = 0 ; 
-  Ttypelist[Tn] = SX ; Tvlist[Tn] =  150.0 ; Tn++ ;
-  Ttypelist[Tn] = SY ; Tvlist[Tn] =   50.0 ; Tn++ ;
-  Ttypelist[Tn] = RZ ; Tvlist[Tn] =   60.0 ; Tn++ ;
-  Ttypelist[Tn] = TX ; Tvlist[Tn] =  140.0 ; Tn++ ;
-  Ttypelist[Tn] = TY ; Tvlist[Tn] =  200.0 ; Tn++ ;
-// Graph only the part with u in [-1, 2]
-	M3d_make_movement_sequence_matrix(A, A_i, Tn, Ttypelist, Tvlist);
-	M3d_mat_mult_points(x[shape], y[shape], z[shape], A, x[shape], y[shape], z[shape], NUM_PTS);
-	graph(x[shape], y[shape], NUM_PTS);
+	T_n = 0;
+	T_type[T_n] = SX;	T_param[T_n] = 150.0;	T_n++;
+	T_type[T_n] = SY;	T_param[T_n] = 50.0;	T_n++;
+	T_type[T_n] = RZ;	T_param[T_n] = 60.0;	T_n++;
+	T_type[T_n] = TX;	T_param[T_n] = 140.0;	T_n++;
+	T_type[T_n] = TY;	T_param[T_n] = 200.0;	T_n++;
+	M3d_make_movement_sequence_matrix(A, A_i, T_n, T_type, T_param);
+	graph_shape(parabola_x, parabola_y, -1, 2, A);
 	G_wait_key();
-	shape++;
 
-
+	// Graph a lemon
 	G_rgb(1, 1, 1);
-	plot_shape(lemon_x, lemon_y, 0, 2 * M_PI, x[shape], y[shape], NUM_PTS);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// lemon :
-//   x^2 - (1 - y^2)^3 = 0
-//
-//   u in [0,2*M_PI] gives the entire lemon :
-//   x = [cos(u)]^3  ;   y = sin(u) ;
-  Tn = 0 ; 
-  Ttypelist[Tn] = SX ; Tvlist[Tn] =  125.0 ; Tn++ ;
-  Ttypelist[Tn] = SY ; Tvlist[Tn] =  125.0 ; Tn++ ;
-  Ttypelist[Tn] = RZ ; Tvlist[Tn] =   60.0 ; Tn++ ;
-  Ttypelist[Tn] = TX ; Tvlist[Tn] =  620.0 ; Tn++ ;
-  Ttypelist[Tn] = TY ; Tvlist[Tn] =  210.0 ; Tn++ ;
-// Graph the entire curve.
-	M3d_make_movement_sequence_matrix(A, A_i, Tn, Ttypelist, Tvlist);
-	M3d_mat_mult_points(x[shape], y[shape], z[shape], A, x[shape], y[shape], z[shape], NUM_PTS);
-	graph(x[shape], y[shape], NUM_PTS);
+	T_n = 0;
+	T_type[T_n] = SX;	T_param[T_n] = 125.0;	T_n++;
+	T_type[T_n] = SY;	T_param[T_n] = 125.0;	T_n++;
+	T_type[T_n] = RZ;	T_param[T_n] = 60.0;	T_n++;
+	T_type[T_n] = TX;	T_param[T_n] = 620.0;	T_n++;
+	T_type[T_n] = TY;	T_param[T_n] = 210.0;	T_n++;
+	M3d_make_movement_sequence_matrix(A, A_i, T_n, T_type, T_param);
+	graph_shape(lemon_x, lemon_y, 0, 2 * M_PI, A);
 	G_wait_key();
-	shape++;
 
-
+	// Graph 3 cycles of a brachistochrone
 	G_rgb(1, 1, 1);
-	plot_shape(brachistochrone_x, brachistochrone_y, 0, 6 * M_PI, x[shape], y[shape], NUM_PTS);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// brachistochrone :
-//   u in [0, tau] gives the entire curve
-// x = u + cos(-pi/2 - u) ; y = 1 + sin(-pi/2 - u);
-	Tn = 0;
-	Ttypelist[Tn] = SX;	Tvlist[Tn] = 20;	Tn++;
-	Ttypelist[Tn] = SY;	Tvlist[Tn] = 20;	Tn++;
-	Ttypelist[Tn] = TX;	Tvlist[Tn] = 100;	Tn++;
-	Ttypelist[Tn] = TY;	Tvlist[Tn] = 30;	Tn++;
-// Graph the entire curve 3 times.
-	M3d_make_movement_sequence_matrix(A, A_i, Tn, Ttypelist, Tvlist);
-	M3d_mat_mult_points(x[shape], y[shape], z[shape], A, x[shape], y[shape], z[shape], NUM_PTS);
-	graph(x[shape], y[shape], NUM_PTS);
+	T_n = 0;
+	T_type[T_n] = SX;	T_param[T_n] = 20;	T_n++;
+	T_type[T_n] = SY;	T_param[T_n] = 20;	T_n++;
+	T_type[T_n] = TX;	T_param[T_n] = 100;	T_n++;
+	T_type[T_n] = TY;	T_param[T_n] = 30;	T_n++;
+	M3d_make_movement_sequence_matrix(A, A_i, T_n, T_type, T_param);
+	graph_shape(brachistochrone_x, brachistochrone_y, 0, 6 * M_PI, A);
 	G_wait_key();
-	shape++;
 
 	while (1)
 		if (G_wait_key() == 'q')
 			return 0;
+
 	return 0;
 }
