28ec386a73e19c43ee223366aa7b37c539e4727f
Linus Brogan <linusbrogan@lclark.edu>
2022-04-08 22:07:52 -0700

    [wip] general 1 or 2 solution solver with some basic test cases

--- lib/polynomial.c
+++ lib/polynomial.c
@@ -4,6 +4,10 @@ double sq(double x) {
 	return x * x;
 }
 
+double cu(double x) {
+	return pow(x, 3);
+}
+
 double isZero(double x) {
 	double epsilon = 1e-10;
 	return fabs(x) < epsilon;
@@ -45,16 +49,49 @@ int solve_quadratic(double a, double b, double c, double x[2]) {
 	return 2;
 }
 
-int solve_depressed_cubic(double c[4], double x[3]) {
-	if (c[2] != 0) return -1;
-	
-}
-
-int solve_cubic(double c[4], double x[3]) {
-	if (c[3] == 0) {
-		return solve_quadratic(c[2], c[1], c[0], x);
+// del Ferro's method
+// Solve y^3 + by + c = 0
+int solve_depressed_cubic(double b, double c, double y[3]) {
+	if (b == 0) {
+		y[0] = cbrt(-c);
+		return 1;
 	}
 
-	return 0;
+	double w[2] = {0, 0};
+	int n = solve_quadratic(1, c, -cu(b) / 27, w);
+	if (n == 0) return 0; // What about complex roots?
+	double z0 = cbrt(w[0]);
+	double z1 = cbrt(w[1]);
+	// What if z = 0? Can't divide then!
+	// If z = 0, then w = 0, so b = 0 (handled above).
+	y[0] = z0 - b / (3 * z0);
+	y[1] = z1 - b / (3 * z1);
+	return 2;
+}
+
+int solve_cubic(double cs[4], double x[3]) {
+	// Non-cubic
+	if (cs[3] == 0) {
+		return solve_quadratic(cs[2], cs[1], cs[0], x);
+	}
+
+	// Cube root
+	if (cs[1] == 0 && cs[2] == 0) {
+		x[0] = cbrt(-cs[0] / cs[3]);
+		return 1;
+	}
+
+	// Use del Ferro's method
+	double b = cs[2] / cs[3];
+	double c = cs[1] / cs[3];
+	double d = cs[0] / cs[3];
+	double bb = -sq(b) / 3 + c;
+	double cc = 2 * cu(b) / 27 - b * c / 3 + d;
+	double y[3];
+	int n = solve_depressed_cubic(bb, cc, y);
+	for (int i = 0; i < n; i++) {
+		x[i] = y[i] - b / 3;
+	}
+	return n;
 }
 
