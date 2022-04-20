3fb89303ed5785249264cfaa0d84e7903db45834
Linus Brogan <linusbrogan@lclark.edu>
2022-04-19 22:18:03 -0700

    Create magnifying glass scene to demonstrate refraction

--- src/raytracer_3d.c
+++ src/raytracer_3d.c
@@ -57,192 +57,90 @@ int main(int argc, char *argv[]) {
 		clear_objects();
 
 		// Configure frame
-		t = 0.1 * frame;
-		double eye[3] = {5, 1, 8};
-		double coi[3] = {0, 0, 5};
-		for (int i = 0; i < 3; i++) {
-			eye[i] += (4 + sin((i + 1) * t)) * (eye[i] - coi[i]);
-		}
-		double up[3] = {eye[_X], eye[_Y], eye[_Z] + 1};
+		double eye[3] = {0, 0, 4};
+		double coi[3] = {0, 0, 0};
+		double up[3] = {eye[_X], eye[_Y] + 1, eye[_Z]};
 
 		// Make view matrix
 		double view[4][4];
 		double view_i[4][4];
 		M3d_view(view, view_i, eye, coi, up);
 
-		double half_width = 5;
-		double half_height = 8;
-
 		// Set light
-		double light[3] = {half_width - 1 + sin(1.2 * t), cos(t), 1.5 * (half_height + sin(t))};
+		double light[3] = {-10, 10, 10};
 		M3d_mat_mult_pt(light_in_eye_space, view, light);
 
-		// Build lower platform
+		// Build image
 		object_type[objects] = OBJ_PLANE;
-		object_color[objects][_R] = 1;
+		object_texture[objects] = TM_MANDELBROT_SET;
+		T_n = 0;
+		T_type[T_n] = TZ;	T_param[T_n] = -10;	T_n++;
+		T_type[T_n] = RZ;	T_param[T_n] = 45;	T_n++;
+		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
+		M3d_mat_mult(object_matrix[objects], view, M);
+		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
+		objects++;
+
+		// Build magnifying glass
+		object_type[objects] = OBJ_SPHERE;
+		object_texture[objects] = TM_SOLID_COLOR;
+		object_color[objects][_R] = 0;
 		object_color[objects][_G] = 1;
 		object_color[objects][_B] = 0.5;
-		object_reflectivity[objects] = 0.7;
-		T_n = 0;
-		T_type[T_n] = SX;	T_param[T_n] = half_width;	T_n++;
-		T_type[T_n] = SY;	T_param[T_n] = half_width;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-		// Build column 1
-		object_type[objects] = OBJ_CYLINDER;
-		object_color[objects][_R] = 1;
-		object_color[objects][_G] = 0;
-		object_color[objects][_B] = 0;
-		T_n = 0;
-		T_type[T_n] = TZ;	T_param[T_n] = 1;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = half_height;	T_n++;
-		T_type[T_n] = TX;	T_param[T_n] = 4;	T_n++;
-		T_type[T_n] = TY;	T_param[T_n] = 4;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-		// Build column 2
-		object_type[objects] = OBJ_CYLINDER;
-		object_color[objects][_R] = 1;
-		object_color[objects][_G] = 0;
-		object_color[objects][_B] = 0;
-		T_n = 0;
-		T_type[T_n] = TZ;	T_param[T_n] = 1;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = half_height;	T_n++;
-		T_type[T_n] = TX;	T_param[T_n] = 4;	T_n++;
-		T_type[T_n] = TY;	T_param[T_n] = -4;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-		// Build column 3
-		object_type[objects] = OBJ_CYLINDER;
-		object_color[objects][_R] = 1;
-		object_color[objects][_G] = 0;
-		object_color[objects][_B] = 0;
-		T_n = 0;
-		T_type[T_n] = TZ;	T_param[T_n] = 1;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = half_height;	T_n++;
-		T_type[T_n] = TX;	T_param[T_n] = -4;	T_n++;
-		T_type[T_n] = TY;	T_param[T_n] = -4;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-		// Build column 4
-		object_type[objects] = OBJ_CYLINDER;
-		object_color[objects][_R] = 1;
-		object_color[objects][_G] = 0;
-		object_color[objects][_B] = 0;
-		T_n = 0;
-		T_type[T_n] = TZ;	T_param[T_n] = 1;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = half_height;	T_n++;
-		T_type[T_n] = TX;	T_param[T_n] = -4;	T_n++;
-		T_type[T_n] = TY;	T_param[T_n] = 4;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-
-		// Build stand base
-		object_type[objects] = OBJ_CYLINDER;
-		object_color[objects][_R] = 0;
-		object_color[objects][_G] = 1;
-		object_color[objects][_B] = 0;
-		T_n = 0;
-		T_type[T_n] = TZ;	T_param[T_n] = 1;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = 0.5;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-		// Build stand
-		object_type[objects] = OBJ_HYPERBOLOID;
-		object_color[objects][_R] = 0;
-		object_color[objects][_G] = 1;
-		object_color[objects][_B] = 0;
-		T_n = 0;
-		T_type[T_n] = RX;	T_param[T_n] = 90;	T_n++;
-		double scale = 1 / cosh(asinh(1));
-		T_type[T_n] = SX;	T_param[T_n] = scale;	T_n++;
-		T_type[T_n] = SY;	T_param[T_n] = scale;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = 2;	T_n++;
-		T_type[T_n] = TZ;	T_param[T_n] = 2 + 1;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-		// Build globe
-		object_type[objects] = OBJ_SPHERE;
-		object_color[objects][_R] = 0;
-		object_color[objects][_G] = 0;
-		object_color[objects][_B] = 0;
 		object_reflectivity[objects] = 0.2;
-		object_texture[objects] = TM_EARTH;
+		object_opacity[objects] = 0.1;
+		object_refractive_index[objects] = 1.5;
 		T_n = 0;
-		T_type[T_n] = NX;	T_param[T_n] = -1;	T_n++;
-		T_type[T_n] = RZ;	T_param[T_n] = -40 * t;	T_n++;
-		T_type[T_n] = SX;	T_param[T_n] = 3;	T_n++;
-		T_type[T_n] = SY;	T_param[T_n] = 3;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = 3;	T_n++;
-		double sphere_center = 1 + 2 * 2 + sqrt(pow(3, 2) - 1);
-		T_type[T_n] = TZ;	T_param[T_n] = sphere_center;	T_n++;
+		double radius = 0.5;
+		T_type[T_n] = SX;	T_param[T_n] = radius;	T_n++;
+		T_type[T_n] = SY;	T_param[T_n] = radius;	T_n++;
+		T_type[T_n] = SZ;	T_param[T_n] = radius / 3;	T_n++;
+		double mag_x = 1.2 * (-1 + 2 * (double) frame / FRAMES);
+		T_type[T_n] = TX;	T_param[T_n] = mag_x;	T_n++;
+		double mag_z = 2;
+		T_type[T_n] = TZ;	T_param[T_n] = mag_z;	T_n++;
 		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
 		M3d_mat_mult(object_matrix[objects], view, M);
 		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
 		objects++;
 
-		// Build hat
-		object_type[objects] = OBJ_CONE;
-		object_color[objects][_R] = 0;
-		object_color[objects][_G] = 0;
-		object_color[objects][_B] = 0;
-		object_texture[objects] = TM_CHECKERBOARD;
-		T_n = 0;
-		T_type[T_n] = NZ;	T_param[T_n] = -1;	T_n++;
-		T_type[T_n] = SX;	T_param[T_n] = 3;	T_n++;
-		T_type[T_n] = SY;	T_param[T_n] = 3;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = 3;	T_n++;
-		T_type[T_n] = RZ;	T_param[T_n] = -40 * t;	T_n++;
-		T_type[T_n] = TZ;	T_param[T_n] = 6 + sphere_center;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-		// Build high halo
+		// Build magnifying glass edge
 		object_type[objects] = OBJ_TORUS;
-		object_color[objects][_R] = 0;
-		object_color[objects][_G] = 0.75;
-		object_color[objects][_B] = 0.9;
+		object_texture[objects] = TM_SOLID_COLOR;
+		object_color[objects][_R] = 0.6;
+		object_color[objects][_G] = 0.6;
+		object_color[objects][_B] = 0.7;
+		object_reflectivity[objects] = 0.1;
+		double params[2] = {1, 0.1};
+		object_parameters[objects] = params;
 		T_n = 0;
-		T_type[T_n] = SX;	T_param[T_n] = 2;	T_n++;
-		T_type[T_n] = SY;	T_param[T_n] = 2;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = 2;	T_n++;
-		T_type[T_n] = TZ;	T_param[T_n] = 13;	T_n++;
+		T_type[T_n] = SX;	T_param[T_n] = radius;	T_n++;
+		T_type[T_n] = SY;	T_param[T_n] = radius;	T_n++;
+		T_type[T_n] = SZ;	T_param[T_n] = 2 * radius / 3;	T_n++;
+		T_type[T_n] = TX;	T_param[T_n] = mag_x;	T_n++;
+		T_type[T_n] = TZ;	T_param[T_n] = mag_z;	T_n++;
 		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
 		M3d_mat_mult(object_matrix[objects], view, M);
 		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
 		objects++;
 
-		// Build low halo
-		object_type[objects] = OBJ_ANNULUS;
-		object_color[objects][_R] = 1;
-		object_color[objects][_G] = 0.75;
+		// Build magnifying glass handle
+		object_type[objects] = OBJ_CYLINDER;
+		object_texture[objects] = TM_SOLID_COLOR;
+		object_color[objects][_R] = 0.75;
+		object_color[objects][_G] = 0.3;
 		object_color[objects][_B] = 0;
 		T_n = 0;
-		T_type[T_n] = NZ;	T_param[T_n] = -1;	T_n++;
-		T_type[T_n] = TZ;	T_param[T_n] = 2;	T_n++;
+		double handle_radius = 0.1;
+		T_type[T_n] = SX;	T_param[T_n] = handle_radius;	T_n++;
+		T_type[T_n] = SY;	T_param[T_n] = handle_radius;	T_n++;
+		double handle_height = radius;
+		T_type[T_n] = TZ;	T_param[T_n] = -1 - radius;	T_n++;
+		T_type[T_n] = RX;	T_param[T_n] = -90;	T_n++;
+		T_type[T_n] = SZ;	T_param[T_n] = handle_height;	T_n++;
+		T_type[T_n] = TZ;	T_param[T_n] = mag_z;	T_n++;
+		T_type[T_n] = RZ;	T_param[T_n] = 30;	T_n++;
+		T_type[T_n] = TX;	T_param[T_n] = mag_x;	T_n++;
 		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
 		M3d_mat_mult(object_matrix[objects], view, M);
 		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
