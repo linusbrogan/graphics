50d7e2c6f8b0446ecce83d65b96e3e04ff1eb77b
Linus Brogan <linusbrogan@lclark.edu>
2022-04-19 19:32:42 -0700

    Add torus to raytracer scene

--- src/raytracer_3d.c
+++ src/raytracer_3d.c
@@ -220,7 +220,22 @@ int main(int argc, char *argv[]) {
 		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
 		objects++;
 
-		// Build halo
+		// Build high halo
+		object_type[objects] = OBJ_TORUS;
+		object_color[objects][_R] = 0;
+		object_color[objects][_G] = 0.75;
+		object_color[objects][_B] = 0.9;
+		T_n = 0;
+		T_type[T_n] = SX;	T_param[T_n] = 2;	T_n++;
+		T_type[T_n] = SY;	T_param[T_n] = 2;	T_n++;
+		T_type[T_n] = SZ;	T_param[T_n] = 2;	T_n++;
+		T_type[T_n] = TZ;	T_param[T_n] = 13;	T_n++;
+		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
+		M3d_mat_mult(object_matrix[objects], view, M);
+		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
+		objects++;
+
+		// Build low halo
 		object_type[objects] = OBJ_ANNULUS;
 		object_color[objects][_R] = 1;
 		object_color[objects][_G] = 0.75;
