7d470fa07c3bf990a05ff21ed0009fe44ff36d72
Linus Brogan <linusbrogan@lclark.edu>
2022-04-19 18:46:16 -0700

    Solve degree 0 and 1 polynomials

--- lib/polynomial.c
+++ lib/polynomial.c
@@ -4,7 +4,26 @@ double sq(double x) {
 	return x * x;
 }
 
+int solve_line(double a, double b, double *x) {
+	if (a == 0) {
+		if (b == 0) return -1;
+		return 0;
+	}
+	*x = -b / a;
+	return 1;
+}
+
 int solve_quadratic(double a, double b, double c, double x[2]) {
+	// Not a quadratic
+	if (a == 0) {
+		int n = solve_line(b, c, x);
+		if (n == 1) {
+			x[1] = x[0];
+		}
+		return n;
+	}
+
+	// Solve
 	double root = b * b - 4 * a * c;
 	if (root < 0) return 0;
 	root = sqrt(root);
