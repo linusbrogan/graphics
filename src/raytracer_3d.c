f33fc00174d5ec12042c075b232074a7833482d8
Linus Brogan <linusbrogan@lclark.edu>
2022-04-21 10:44:11 -0700

    [WIP] Make glass of water with straw

--- src/raytracer_3d.c
+++ src/raytracer_3d.c
@@ -57,10 +57,10 @@ int main(int argc, char *argv[]) {
 		clear_objects();
 
 		// Configure frame
-		double eye[3] = {0, 0, 4};
+		double eye[3] = {0.5, 4, 1};
 		double coi[3] = {0, 0, 0};
-		double up[3] = {eye[_X], eye[_Y] + 1, eye[_Z]};
-		double eye_spacing = 0.02;
+		double up[3] = {eye[_X], eye[_Y], eye[_Z] + 1};
+		double eye_spacing = 0.2;
 
 		// Make view matrix
 		double view[4][4];
@@ -71,77 +71,60 @@ int main(int argc, char *argv[]) {
 		double light[3] = {-10, 10, 10};
 		M3d_mat_mult_pt(light_in_eye_space, view, light);
 
-		// Build image
-		object_type[objects] = OBJ_PLANE;
-		object_texture[objects] = TM_MANDELBROT_SET;
-		T_n = 0;
-		T_type[T_n] = TZ;	T_param[T_n] = -10;	T_n++;
-		T_type[T_n] = RZ;	T_param[T_n] = 45;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-		// Build magnifying glass
-		object_type[objects] = OBJ_SPHERE;
+		// Build glass
+		object_type[objects] = OBJ_CYLINDER;
 		object_texture[objects] = TM_SOLID_COLOR;
 		object_color[objects][_R] = 0;
 		object_color[objects][_G] = 1;
-		object_color[objects][_B] = 0.5;
+		object_color[objects][_B] = 0.8;
 		object_reflectivity[objects] = 0.2;
 		object_opacity[objects] = 0.1;
 		object_refractive_index[objects] = 1.5;
 		T_n = 0;
-		double radius = 0.5;
-		T_type[T_n] = SX;	T_param[T_n] = radius;	T_n++;
-		T_type[T_n] = SY;	T_param[T_n] = radius;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = radius / 3;	T_n++;
-		double mag_x = 1.2 * (-1 + 2 * (double) frame / FRAMES);
-		T_type[T_n] = TX;	T_param[T_n] = mag_x;	T_n++;
-		double mag_z = 2;
-		T_type[T_n] = TZ;	T_param[T_n] = mag_z;	T_n++;
+		T_type[T_n] = TZ;	T_param[T_n] = 1;	T_n++;
+		T_type[T_n] = SX;	T_param[T_n] = 0.5;	T_n++;
+		T_type[T_n] = SY;	T_param[T_n] = 0.5;	T_n++;
+		T_type[T_n] = SZ;	T_param[T_n] = 0.5;	T_n++;
 		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
 		M3d_mat_mult(object_matrix[objects], view, M);
 		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
 		objects++;
 
-		// Build magnifying glass edge
-		object_type[objects] = OBJ_TORUS;
-		object_texture[objects] = TM_SOLID_COLOR;
-		object_color[objects][_R] = 0.6;
-		object_color[objects][_G] = 0.6;
-		object_color[objects][_B] = 0.7;
-		object_reflectivity[objects] = 0.1;
-		double params[2] = {1, 0.1};
-		object_parameters[objects] = params;
-		T_n = 0;
-		T_type[T_n] = SX;	T_param[T_n] = radius;	T_n++;
-		T_type[T_n] = SY;	T_param[T_n] = radius;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = 2 * radius / 3;	T_n++;
-		T_type[T_n] = TX;	T_param[T_n] = mag_x;	T_n++;
-		T_type[T_n] = TZ;	T_param[T_n] = mag_z;	T_n++;
-		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
-		M3d_mat_mult(object_matrix[objects], view, M);
-		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
-		objects++;
-
-		// Build magnifying glass handle
+		// Build water
 		object_type[objects] = OBJ_CYLINDER;
 		object_texture[objects] = TM_SOLID_COLOR;
-		object_color[objects][_R] = 0.75;
+		object_color[objects][_R] = 0;
 		object_color[objects][_G] = 0.3;
-		object_color[objects][_B] = 0;
+		object_color[objects][_B] = 1;
+		object_reflectivity[objects] = 0.2;
+		object_opacity[objects] = 0.4;
+		object_refractive_index[objects] = 1.5;
 		T_n = 0;
-		double handle_radius = 0.1;
-		T_type[T_n] = SX;	T_param[T_n] = handle_radius;	T_n++;
-		T_type[T_n] = SY;	T_param[T_n] = handle_radius;	T_n++;
-		double handle_height = radius;
-		T_type[T_n] = TZ;	T_param[T_n] = -1 - radius;	T_n++;
-		T_type[T_n] = RX;	T_param[T_n] = -90;	T_n++;
-		T_type[T_n] = SZ;	T_param[T_n] = handle_height;	T_n++;
-		T_type[T_n] = TZ;	T_param[T_n] = mag_z;	T_n++;
-		T_type[T_n] = RZ;	T_param[T_n] = 30;	T_n++;
-		T_type[T_n] = TX;	T_param[T_n] = mag_x;	T_n++;
+		T_type[T_n] = TZ;	T_param[T_n] = 1;	T_n++;
+		T_type[T_n] = SX;	T_param[T_n] = 0.45;	T_n++;
+		T_type[T_n] = SY;	T_param[T_n] = 0.45;	T_n++;
+		T_type[T_n] = SZ;	T_param[T_n] = 0.3;	T_n++;
+		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
+		M3d_mat_mult(object_matrix[objects], view, M);
+		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
+		objects++;
+
+		// Build straw
+		object_type[objects] = OBJ_CYLINDER;
+		object_texture[objects] = TM_SOLID_COLOR;
+		object_color[objects][_R] = 1;
+		object_color[objects][_G] = 0;
+		object_color[objects][_B] = 0;
+		object_reflectivity[objects] = 0.1;
+		object_opacity[objects] = 0.9;
+		object_refractive_index[objects] = 1.4;
+		T_n = 0;
+		T_type[T_n] = TZ;	T_param[T_n] = 1.3;	T_n++;
+		T_type[T_n] = SX;	T_param[T_n] = 0.05;	T_n++;
+		T_type[T_n] = SY;	T_param[T_n] = 0.05;	T_n++;
+		T_type[T_n] = SZ;	T_param[T_n] = 0.5;	T_n++;
+		T_type[T_n] = RY;	T_param[T_n] = 30;	T_n++;
+		T_type[T_n] = TX;	T_param[T_n] = -0.25;	T_n++;
 		M3d_make_movement_sequence_matrix(M, M_i, T_n, T_type, T_param);
 		M3d_mat_mult(object_matrix[objects], view, M);
 		M3d_mat_mult(object_matrix_i[objects], M_i, view_i);
